ba4e6d63f2280945e9c544f5dec4164a70250ea3
bethwelkipruto <bethwel.c7@gmail.com>
2025-06-23 14:40:45 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+// Declare customerName in global scope
+var customerName = 'bob';
+
+// Function to uppercase customerName
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// Declare bestCustomer in global scope
+var bestCustomer;
+
+// Function to set bestCustomer
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+// Function to overwrite bestCustomer
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+// Declare leastFavoriteCustomer with const
+const leastFavoriteCustomer = 'someone';
+
+// Function to try to change leastFavoriteCustomer
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'someone else'; // This will throw an error
+}
